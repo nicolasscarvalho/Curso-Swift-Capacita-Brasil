ce95550cdf347108603e6b7146045e17d9fcb85a
Nícolas Carvalho <98130635+nicolasscarvalho@users.noreply.github.com>
2024-10-05 23:12:09 +0000

    feat: strings

---
+++ 5_strings.swift
@@ -0,0 +1,11 @@
+let sentence: String = #"This a sentence without "special characters" in print\n"#
+let longText: String = """
+================= This is a long text =================
+(A) Option 1
+(B) Option 2
+(C) Option 3
+(D) Option 4
+"""
+
+print(sentence)
+print(longText)
